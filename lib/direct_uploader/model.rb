fdc3360f113b908e6ba1f9088363bbaccd1cfef9
arnaud sellenet <arnodmental@gmail.com>
2018-06-07 12:18:07 +0200

    add active_support dependency

--- lib/direct_uploader/model.rb
+++ lib/direct_uploader/model.rb
@@ -1,3 +1,5 @@
+require "active_support/concern"
+
 module DirectUploader
   module Model
     extend ActiveSupport::Concern
--- spec/direct_uploader_spec.rb
+++ spec/direct_uploader_spec.rb
@@ -1,11 +1,7 @@
 require "spec_helper"
 
-RSpec.describe Directuploader do
+RSpec.describe DirectUploader do
   it "has a version number" do
-    expect(Directuploader::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(DirectUploader::VERSION).not_to be nil
   end
 end
