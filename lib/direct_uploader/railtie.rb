71deedaadcfa3abdc147e8846bc8ef9b9ece7b6c
arnaud sellenet <arnodmental@gmail.com>
2018-06-11 10:55:28 +0200

    Initialize configuration variable in railtie

--- lib/direct_uploader/railtie.rb
+++ lib/direct_uploader/railtie.rb
@@ -4,6 +4,7 @@ module DirectUploader
   class Railtie < Rails::Railtie
     initializer "direct_uploader.view_helpers" do
       ActionView::Base.send :include, ViewHelpers
+      Rails.configuration.define_singleton_method(:direct_uploader_adapter) { Rails.env.test? ? DirectUploader::Adapter::FileSystem : DirectUploader::Adapter::S3 }
     end
   end
 end
