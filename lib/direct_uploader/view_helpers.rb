25267f9ac5f40764a4fc4fa319ccc171e2dc5958
arnaud sellenet <arnodmental@gmail.com>
2018-06-14 15:13:49 +0200

    better portability for view_helper

--- lib/direct_uploader/view_helpers.rb
+++ lib/direct_uploader/view_helpers.rb
@@ -1,15 +1,16 @@
 module DirectUploader
   module ViewHelpers
     def directupload_field_for(f, field, options = {})
-      data = { 'form-data' => f.object.presigned_post[:fields],
-               'url'       => f.object.presigned_post[:url],
-               'host'      => URI.parse(f.object.presigned_post[:url]).host
+      object = f.object.to_model
+      data = { 'form-data' => object.presigned_post[:fields],
+               'url'       => object.presigned_post[:url],
+               'host'      => URI.parse(object.presigned_post[:url]).host
       }
-      if f.object.direct_uploader_field_options[field][:max_file_size].present?
-        data['max_file_size'] = f.object.direct_uploader_field_options[field][:max_file_size]
+      if object.direct_uploader_field_options[field][:max_file_size].present?
+        data['max_file_size'] = object.direct_uploader_field_options[field][:max_file_size]
       end
-      if f.object.direct_uploader_field_options[field][:file_type].present?
-        data['file_type'] = Array(f.object.direct_uploader_field_options[field][:file_type]).join("|")
+      if object.direct_uploader_field_options[field][:file_type].present?
+        data['file_type'] = Array(object.direct_uploader_field_options[field][:file_type]).join("|")
       end
 
       if options.has_key?(:autosubmit) && options[:autosubmit]
@@ -22,7 +23,7 @@ module DirectUploader
             class: "directUpload",
             data: data
           },
-          hint: uploader_hint(f.object, field, options)
+          hint: uploader_hint(object, field, options)
       )
     end
 
