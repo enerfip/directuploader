f00eec8cf3a7f38923eb694776598ff2f4ff9df9
Majid <majid.jaidi@gmail.com>
2025-08-06 10:26:10 +0200

    allow custom s3 endpoint

--- lib/direct_uploader/adapter/s3.rb
+++ lib/direct_uploader/adapter/s3.rb
@@ -4,7 +4,7 @@ module DirectUploader
       def presigned_post(hsh)
         start_path = File.dirname(hsh[:key])
         {
-          url:    "https://#{S3_PRIVATE_BUCKET.key}.s3.#{S3_CONNECTION.region}.amazonaws.com",
+          url:    S3_ENDPOINT.presence || "https://#{S3_PRIVATE_BUCKET.key}.s3.#{S3_CONNECTION.region}.amazonaws.com",
           fields: S3_CONNECTION.post_object_hidden_fields(hsh.stringify_keys.except("expires_in").merge "policy" => {
             "conditions" => [{"bucket" => S3_PRIVATE_BUCKET.key}, hsh.stringify_keys.except("key", "expires_in"), ["starts-with", "$key", start_path]],
             "expiration" => (Time.zone.now + hsh.fetch(:expires_in) { 5.minutes }).utc.iso8601
